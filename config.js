33b14ea54b03b97f4a3ecfa18b4e3810d1c66692
Sparks ⵢ <120671421+Sparks33@users.noreply.github.com>
2023-12-20 14:34:12 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    modmail: {
+        ownerID: '782818581485846549', // ← Insert the Server Owner ID.
+        supportId: '1186624119685521418', // ← Insert General Support Category ID.
+        premiumId: '1186624172214976523', // ← Insert Premium Support Category ID.
+        whitelist: '1186625005501566976', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1186625043560661022', // ← Insert Staff Role to be pinged upon ticket creation.
+    },
+    logs: {
+        logschannel: '1186625617400172544' // ← Insert Ticket Logs Channel ID.
+    }
+};
